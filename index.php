47fb23535e7d302f0c01441abb9012a6c1f77291
glen leao <glenleao@gmail.com>
2019-08-07 20:58:04 -0300

    autoload kgdzoito classes

--- index.php
+++ index.php
@@ -8,7 +8,11 @@ $app->config('debug', true);
 
 $app->get('/', function() {
     
-	echo "OK";
+	// echo "OK - nova loja ecommerce";
+	$sql = new Kgdzoito\DB\Sql();
+	$results = $sql->select("SELECT * FROM tb_users");
+
+	echo json_encode($results);
 
 });
 
--- vendor/composer/autoload_static.php
+++ vendor/composer/autoload_static.php
@@ -6,8 +6,18 @@ namespace Composer\Autoload;
 
 class ComposerStaticInit2e70b232e8f78e84ea35fc7a39b2bee1
 {
-    public static $fallbackDirsPsr4 = array (
-        0 => __DIR__ . '/..' . '/hcodebr/php-classes/src',
+    public static $prefixLengthsPsr4 = array (
+        'K' => 
+        array (
+            'Kgdzoito\\' => 9,
+        ),
+    );
+
+    public static $prefixDirsPsr4 = array (
+        'Kgdzoito\\' => 
+        array (
+            0 => __DIR__ . '/..' . '/hcodebr/php-classes/src',
+        ),
     );
 
     public static $prefixesPsr0 = array (
@@ -41,7 +51,8 @@ class ComposerStaticInit2e70b232e8f78e84ea35fc7a39b2bee1
     public static function getInitializer(ClassLoader $loader)
     {
         return \Closure::bind(function () use ($loader) {
-            $loader->fallbackDirsPsr4 = ComposerStaticInit2e70b232e8f78e84ea35fc7a39b2bee1::$fallbackDirsPsr4;
+            $loader->prefixLengthsPsr4 = ComposerStaticInit2e70b232e8f78e84ea35fc7a39b2bee1::$prefixLengthsPsr4;
+            $loader->prefixDirsPsr4 = ComposerStaticInit2e70b232e8f78e84ea35fc7a39b2bee1::$prefixDirsPsr4;
             $loader->prefixesPsr0 = ComposerStaticInit2e70b232e8f78e84ea35fc7a39b2bee1::$prefixesPsr0;
             $loader->classMap = ComposerStaticInit2e70b232e8f78e84ea35fc7a39b2bee1::$classMap;
 
