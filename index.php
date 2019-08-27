9c0cab7c153a0e28ade41f808a7dbea58e84752d
glenleao <glenleao@gmail.com>
2019-08-26 22:37:00 -0300

    novo commit

--- index.php
+++ index.php
@@ -86,11 +86,20 @@ $app->get("/admin/users/:iduser/delete", function($iduser){
 
 	User::verifyLogin();
 
+	$user = new User();
+	$user->get((int)$iduser);
+	$user->delete();
+	header("Location: /admin/users");
+	exit;
+
 });
 
 $app->get("/admin/users/:iduser", function($iduser){
+
 	User::verifyLogin();
+
 	$user = new User();
+
 	$user->get((int)$iduser);
 	$page = new PageAdmin();
 	$page->setTpl("users-update", array(
@@ -100,11 +109,28 @@ $app->get("/admin/users/:iduser", function($iduser){
 
 $app->post("/admin/users/create", function(){
 	User::verifyLogin();
-	var_dump($_POST);
+	
+	$user = new User();
+
+	$_POST["inadmin"] = (isset($_POST["inadmin"]))?1:0;
+
+	$user->setData($_POST);
+	$user->save();
+	header("Location: /admin/users");
+	exit;
 });
 
 $app->post("/admin/users/:iduser", function($iduser){
 	User::verifyLogin();
+
+	$user = new User();
+	$_POST["inadmin"] = (isset($_POST["inadmin"]))?1:0;
+
+	$user->get((int)$iduser);
+	$user->setData($_POST);
+	$user->update();
+	header("Location: /admin/users");
+	exit;
 });
 
 
